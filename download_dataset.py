6f14abe24f28f7aa378cba68bc45ec41ec64b52d
Jervis <jervismuindi@yahoo.co.uk>
2019-05-28 16:15:51 -0700

    Update download dataset script

--- download_dataset.py
+++ download_dataset.py
@@ -4,10 +4,12 @@
 import tensorflow_datasets as tfds
 
 def main():
+    print ("Starting data download....")
     emnist_builder = tfds.builder("emnist")
     emnist_builder.download_and_prepare()
     emnist_info = emnist_builder.info
-    print (emnist_info)
+    print(emnist_info)
+    print("Download COMPLETE !")
 
 
 if __name__ == '__main__':
