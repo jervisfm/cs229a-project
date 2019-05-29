4022b77acdc04337ad84a00815ce3ed0d993a319
Jervis <jervismuindi@yahoo.co.uk>
2019-05-28 17:55:15 -0700

    First pass attempt. get confusion matrix w/o letter labels and only 1-10

--- data_loader.py
+++ data_loader.py
@@ -29,22 +29,39 @@ X_train = X_train[0:val_start,:]
 y_train = y_train[0:val_start]
 
 
-X_train = X_train.reshape( (X_train.shape[0], 28, 28), order='F')
-X_val = X_val.reshape( (X_val.shape[0], 28, 28), order='F')
-X_test = X_test.reshape( (X_test.shape[0], 28, 28), order='F')
+print ("Intialize x shape:", X_train.shape)
+print("Initial y shape ", y_train[23])
+X_train_images = X_train.reshape( (X_train.shape[0], 28, 28), order='F')
+X_val_images = X_val.reshape( (X_val.shape[0], 28, 28), order='F')
+X_test_images = X_test.reshape( (X_test.shape[0], 28, 28), order='F')
+
+
+#X_train_vectors = X_train.reshape( (28, 28, 1, 784), order='F')
+#X_val_vectors = X_val.reshape( (28, 28, 1, 784), order='F')
+#X_test_vectors = X_test.reshape( (28, 28, 1, 784), order='F')
 
 
 
 
-def get_train():
-    return X_train, y_train
 
-def get_dev():
-    return X_val, y_val
+def get_train(as_vectors=False):
+    if as_vectors:
+        return X_train, y_train
+    else:
+      return X_train_images, y_train
 
-def get_test():
-    return X_test, y_test
+def get_dev(as_vectors=False):
+    if as_vectors:
+        return X_val, y_val
+    else:
+        return X_val_images, y_val
 
+def get_test(as_vectors=False):
+
+    if as_vectors:
+        return X_test, y_test
+    else:
+        return X_test_images, y_test
 
 
 if __name__ == '__main__':
--- logistic_regression_model.py
+++ logistic_regression_model.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report
 
 import tensorflow as tf
 import util
+import data_loader
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -55,15 +56,15 @@ def run():
 
     # TODO: Load training training data.
 
-    #X_train, Y_train = dataGetter.getTrain()
-    #X_dev, Y_dev = dataGetter.getDev()
+    X_train, Y_train = data_loader.get_train(as_vectors=True)
+    X_dev, Y_dev = data_loader.get_dev(as_vectors=True)
 
     start_time_secs = time.time()
     print("Starting Logistic Regression training ...")
     clf = LogisticRegression(random_state=0,
                              solver='lbfgs',
                              multi_class='multinomial',
-                             verbose=10,
+                             verbose=1,
                              n_jobs=-1,
                              max_iter=FLAGS.max_iter).fit(X_train, Y_train)
     print("Training done.")
@@ -83,19 +84,29 @@ def run():
         1.0 * training_duration_secs / FLAGS.max_iter)
 
     class_names = util.get_label(Y_dev)
-    classification_report_string = classification_report(Y_dev, Y_dev_prediction, target_names=class_names)
+    # TODO: fix target names
+    #classification_report_string = classification_report(Y_dev, Y_dev_prediction, target_names=class_names)
+    classification_report_string = classification_report(Y_dev, Y_dev_prediction)
+
     experiment_result_string += "\nClassification report: {}".format(classification_report_string)
 
     print(experiment_result_string)
 
     # Save report to file
     util.write_contents_to_file(get_experiment_report_filename(), experiment_result_string)
-    confusion = confusion_matrix(Y_dev, Y_dev_prediction, labels=class_names)
+
+    # Fix class names
+    #confusion = confusion_matrix(Y_dev, Y_dev_prediction, labels=class_names)
+    confusion = confusion_matrix(Y_dev, Y_dev_prediction)
 
     print("Confusion matrix: ", confusion)
     pickle.dump(class_names, open(get_class_filename(), 'wb'))
     pickle.dump(confusion, open(get_confusion_matrix_filename(), 'wb'))
 
+    # TODO: plot confusion matrix
+    confusion = confusion[:10, :10]
+    class_names = class_names[:10]
+    util.create_confusion_matrices(class_names, confusion, get_confusion_matrix_name())
 
 def main():
     class_names = pickle.load(open(get_class_filename(), 'rb'))[:10]
@@ -104,4 +115,6 @@ def main():
 
 
 if __name__ == '__main__':
+
     run()
+
