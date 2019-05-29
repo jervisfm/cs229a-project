c471b91016465493e42c3814ad0493086e40e79d
Jervis <jervismuindi@yahoo.co.uk>
2019-05-28 22:50:05 -0700

    Modify init script

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p data/matlab
+mkdir -p results
+mkdir -p imgs/cm_imgs
 wget "https://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/matlab.zip" -O data/matlab.zip
 unzip data/matlab.zip -d data/
