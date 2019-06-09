7ecb291efdb72cf830fac581d3b15a943be3668d
Jervis <jervismuindi@yahoo.co.uk>
2019-06-08 22:08:03 -0700

    Add helper method used in visualize dataset:

--- data_loader.py
+++ data_loader.py
@@ -23,10 +23,12 @@ y_test = data['test'][0,0]['labels'][0,0]
 
 
 val_start = X_train.shape[0] - X_test.shape[0]
+
 X_val = X_train[val_start:X_train.shape[0],:]
 y_val = y_train[val_start:X_train.shape[0]]
 X_train = X_train[0:val_start,:]
 y_train = y_train[0:val_start]
+
 
 
 print ("Intialize x train shape:", X_train.shape)
@@ -34,6 +36,7 @@ print ("Intialize x dev shape:", X_val.shape)
 print ("Intialize x test shape:", X_test.shape)
 
 print('Y train labels', util.get_label(y_train))
+
 X_train_images = X_train.reshape( (X_train.shape[0], 28, 28), order='F')
 X_val_images = X_val.reshape( (X_val.shape[0], 28, 28), order='F')
 X_test_images = X_test.reshape( (X_test.shape[0], 28, 28), order='F')
@@ -75,3 +78,6 @@ if __name__ == '__main__':
     print('Y_dev shape:', y_val.shape)
     print('X_test shape:', X_test.shape)
     print('Y_test shape:', y_test.shape)
+
+
+print('val start', val_start)
